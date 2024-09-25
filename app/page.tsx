f775bc9d239dc04f8630504f8449fb865639df5a
chandramoulisangabathula01 <127196627+chandramoulisangabathula01@users.noreply.github.com>
2024-09-25 21:10:22 +0530

    404 added and case-studies setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,12 @@
 "use client";
-
-// import { navItems } from "@/data";
-
 import Hero from "@/components/Hero";
-// import Grid from "@/components/OurCoreServices";
 import Footer from "@/components/Footer";
-// import Clients from "@/components/Clients";
-// // import Approach from "@/components/Approach";
-// import Experience from "@/components/OurServices";
-// import RecentProjects from "@/components/RecentProjects";
-// import { FloatingNav } from "@/components/ui/FloatingNavbar";
 import Navbar from "@/components/NavBar";
-// import NavbarTop from "@/components/ui/navbartop";
 import { TabsDemo } from "@/components/services";
-// import { StickyScrollRevealDemo } from "@/components/CoreServices";
-// import Trial from "@/components/Trial";
 import { MarqueeDemo } from "@/components/Logoscroll";
-// import { motion } from "framer-motion";
 import OurServices from "@/components/OurServices";
 import CoreServices from "@/components/OurCoreServices";
 
-// import { TimelineDemo } from "@/components/CoreServices";
-// import Services from "@/components/services";
 
 const Home = () => {
   return (
@@ -30,6 +15,7 @@ const Home = () => {
         className="relative dark:bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 no-scrollbar"
         style={{
           backgroundColor: "#faefe0",
+          // backgroundColor: "#DEE5D4",
           // background: "linear-gradient(180deg, #faefe0,#fff7ad)",
         }}
       >
@@ -40,15 +26,6 @@ const Home = () => {
           <OurServices />
           <CoreServices services={[]} />
           <TabsDemo />
-          {/* <FloatingNav navItems={navItems} /> */}
-          {/* <NavbarTop /> */}
-          {/* <ServicesGsap/> */}
-          {/* <Trial/> */}
-          {/* <StickyScrollRevealDemo /> */}
-          {/* <TimelineDemo/> */}
-          {/* <Approach /> */}
-          {/* <RecentProjects /> */}
-          {/* <Clients /> */}
         </div>
       </main>
       <Footer />
